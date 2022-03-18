3c0918e564e8cb722e2f097f6b7d48e47a3cd4d6
sahin <sahin.kasap@sirenbilisim.com.tr>
2022-03-19 00:43:22 +0300

    mastercard and visa pngs uploaded and they are shown in UI

--- ui/src/CreditCardInput.tsx
+++ ui/src/CreditCardInput.tsx
@@ -1,26 +1,57 @@
 import { useState } from "react";
 import "./App.css";
-import './card.css'
+import "./card.css";
 
-interface ICreditCardInputProps{
+interface ICreditCardInputProps {}
+export function CreditCardInput(props: ICreditCardInputProps) {
+  const [frontFaceText, setFrontFaceText] = useState("");
+  const [frontFaceShownText, setFrontFaceShownText] = useState("423");
+  return (
+    <div className="row">
+      <div className="col-md-6 d-flex justify-content-center">
+        <div id="wrapper">
+          {frontFaceShownText[0] === "4" && (
+            <img
+              src="visa.png"
+              style={{
+                top: "0px",
+                left: "20px",
+                height: "100px",
+                position: "absolute",
+                zIndex: "1",
+              }}
+            />
+          )}
+          {frontFaceShownText[0] === "5" && (
+            <img
+              src="mastercard.png"
+              style={{
+                top: "10px",
+                left: "10px",
+                height: "100px",
+                position: "absolute",
+                zIndex: "1",
+              }}
+            />
+          )}
+          <img className="creditcard" src="front.png" />
 
-}
-export function CreditCardInput(props: ICreditCardInputProps){
-    const [frontFaceText,setFrontFaceText] = useState("");
-    const [frontFaceShownText,setFrontFaceShownText] = useState("123qwe");
-    return <div className="row">
-    <div  className="col-md-6 d-flex justify-content-center">
-      <div id="wrapper">
-        <img className="creditcard" src="front.png" />
-        <input
-          style={{ position: "absolute", top: "152px", left: "36px" , width:"250px"} }
-          value={frontFaceShownText}
-          onChange={(event)=>{
+          <input
+            type="number"
+            style={{
+              position: "absolute",
+              top: "152px",
+              left: "36px",
+              width: "250px",
+            }}
+            value={frontFaceShownText}
+            onChange={(event) => {
               console.log(event.target.value);
-              console.log(event.target.selectionStart)
-          }}
-        ></input>
+              console.log(event.target.selectionStart);
+            }}
+          ></input>
         </div>
+      </div>
     </div>
-  </div>
+  );
 }
