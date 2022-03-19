fa7596a6d26383499e1698c82260059064eef95f
sahin <sahin.kasap@sirenbilisim.com.tr>
2022-03-19 10:34:32 +0300

    cvc ve date olarak iki input eklendi

--- ui/src/CreditCardInput.tsx
+++ ui/src/CreditCardInput.tsx
@@ -3,85 +3,144 @@ import "./App.css";
 import "./card.css";
 import { p } from "./util/console-log";
 import { splice } from "./util/string";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 interface ICreditCardInputProps {}
 export function CreditCardInput(props: ICreditCardInputProps) {
   const [frontFaceText, setFrontFaceText] = useState("");
+  const [cvc, setCvc] = useState("");
+  const [date, setDate] = useState("");
   const [frontFaceShownText, setFrontFaceShownText] = useState("423");
   return (
-    <div className="row">
-      <div className="col-md-6 d-flex justify-content-center">
-        <div id="wrapper">
-          {frontFaceShownText[0] === "4" && (
-            <img
-              src="visa.png"
-              style={{
-                top: "0px",
-                left: "20px",
-                height: "100px",
-                position: "absolute",
-                zIndex: "1",
-              }}
-            />
-          )}
-          {frontFaceShownText[0] === "5" && (
-            <img
-              src="mastercard.png"
-              style={{
-                top: "10px",
-                left: "10px",
-                height: "100px",
-                position: "absolute",
-                zIndex: "1",
-              }}
-            />
-          )}
-          <img className="creditcard" src="front.png" />
+    <>
+      <div className="row">
+        <div className="col-md-12 d-flex justify-content-center">
+          <div id="wrapper">
+            {frontFaceShownText[0] === "4" && (
+              <img
+                src="visa.png"
+                style={{
+                  top: "0px",
+                  left: "20px",
+                  height: "100px",
+                  position: "absolute",
+                  zIndex: "1",
+                }}
+              />
+            )}
+            {frontFaceShownText[0] === "5" && (
+              <img
+                src="mastercard.png"
+                style={{
+                  top: "10px",
+                  left: "10px",
+                  height: "100px",
+                  position: "absolute",
+                  zIndex: "1",
+                }}
+              />
+            )}
+            <img className="creditcard" src="front.png" />
 
-          <p
-          style={({position: "absolute",top: "152px",
-          left: "36px",
-          width: "250px",})}
-          >{frontFaceShownText}</p>
+            <p
+              style={{
+                position: "absolute",
+                top: "152px",
+                left: "36px",
+                width: "250px",
+              }}
+            >
+              {frontFaceShownText}
+            </p>
+          </div>
         </div>
-        <input
-            type='number'
+      </div>
+      <div className="row">
+        <div className="col-md-12 d-flex justify-content-center">
+          <button className="btn btn-primary"> Arka Yüzü Çevir </button>
+        </div>
+      </div>
+      <div className="row d-flex justify-content-center">
+        <div className="col-md-6 d-flex justify-content-center">
+          <input
+            type="number"
             maxLength={16}
             style={{
-                margin:'auto',
-              position: "absolute",
-              top: "152px",
-              left: "36px",
-              width: "250px",
+              margin: "auto",
+              width: "100%",
+              //   width: "250px",
             }}
-            placeholder='Credit Card Number'
+            placeholder="Credit Card Number"
             name="cardinput"
             value={frontFaceText}
             onChange={(event) => {
-                console.log(event.target.value);
-                if(event.target.value.length>16){
-                    event.preventDefault();
-                    return;
-                }
-                setFrontFaceShownText(replaceWithAposthrophs(event.target.value))
-                setFrontFaceText(event.target.value);
-                
-                
-                
-                
+              console.log(event.target.value);
+              if (event.target.value.length > 16) {
+                event.preventDefault();
+                return;
+              }
+              setFrontFaceShownText(replaceWithAposthrophs(event.target.value));
+              setFrontFaceText(event.target.value);
             }}
-
           ></input>
+        </div>
       </div>
-    </div>
+      <div className="row d-flex justify-content-center">
+        <div className="col-md-6 d-flex justify-content-center">
+          <input
+            type="number"
+            maxLength={3}
+            style={{
+              margin: "auto",
+              width: "100%",
+              //   width: "250px",
+            }}
+            placeholder="CVC"
+            name="cvc"
+            value={cvc}
+            onChange={(event) => {
+              console.log(event.target.value);
+              if (event.target.value.length > 3) {
+                event.preventDefault();
+                return;
+              }
+              setCvc(event.target.value);
+            }}
+          ></input>
+          <input
+            type="number"
+            maxLength={3}
+            style={{
+              margin: "auto",
+              width: "100%",
+              //   width: "250px",
+            }}
+            placeholder="Date"
+            name="date"
+            value={date}
+            onChange={(event) => {
+              console.log(event.target.value);
+              if (event.target.value.length > 5) {
+                event.preventDefault();
+                return;
+              }
+              setDate(event.target.value);
+            }}
+          ></input>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-12 d-flex justify-content-center">
+          <button className="btn btn-success"> Submit </button>
+        </div>
+      </div>
+    </>
   );
 }
 function replaceWithAposthrophs(value: string): string {
-    let len = value.length;
-    let splitted = value.split('')
-    for(let i=4;i<len && i<12;i++){
-        splitted[i]='*'
-    }
-    return splitted.join('');
+  let len = value.length;
+  let splitted = value.split("");
+  for (let i = 4; i < len && i < 12; i++) {
+    splitted[i] = "*";
+  }
+  return splitted.join("");
 }
-
