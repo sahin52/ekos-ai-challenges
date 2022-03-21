f7380e76d2ef2044511b84101b463dc6dd7fd6c9
sahin <sahin.kasap@sirenbilisim.com.tr>
2022-03-21 23:10:18 +0300

    number of random samples

--- be/src/service/recommendation/recommendationService.ts
+++ be/src/service/recommendation/recommendationService.ts
@@ -4,7 +4,7 @@ import { Request } from 'express';
 
 
 export async function recommend(req: Request) {
-    let res = await Book.aggregate().sample(1);
+    let res = await Book.aggregate().sample(Math.floor(Math.random()*4+2));
     console.log(res);
     return res;
 }
